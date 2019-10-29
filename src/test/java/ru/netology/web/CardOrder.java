b6a22b4181233325a4034e3210bb21d3e5a256f9
Margarita <sov-team@ya.ru>
2019-10-29 16:25:02 +0700

    add Parametrized

--- src/test/java/ru/netology/web/CardOrder.java
+++ src/test/java/ru/netology/web/CardOrder.java
@@ -2,6 +2,9 @@ package ru.netology.web;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -20,7 +23,7 @@ public class CardOrder {
                                                                           "свяжется с вами в ближайшее время."));
     }
     @Test
-    @DisplayName("Не заполенено поле Имя, ошибка")
+    @DisplayName("Не заполнено поле Имя, ошибка")
     void shouldTestEmptyName() {
         open("http://localhost:9999/");
         $("[data-test-id=phone] input.input__control").setValue("+79270000000");
@@ -29,7 +32,7 @@ public class CardOrder {
         $(".input_invalid").shouldHave(exactText("Фамилия и имя Поле обязательно для заполнения"));
     }
     @Test
-    @DisplayName("Не заполенено поле Телефон, ошибка")
+    @DisplayName("Не заполнено поле Телефон, ошибка")
     void shouldTestEmptyPhoneNumber() {
         open("http://localhost:9999/");
         $("[data-test-id=name] input.input__control").setValue("Пупкин Василий");
@@ -37,23 +40,24 @@ public class CardOrder {
         $("button").click();
         $(".input_invalid").shouldHave(exactText("Мобильный телефон Поле обязательно для заполнения"));
     }
-    @Test
-    @DisplayName("Поле Имя заполенно не корректно, ошибка")
-    void shouldTestIrrelevantName() {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/IrrelevantName2.csv", numLinesToSkip = 1)
+    void shouldTestIrrelevantName(String name, String phoneNumber, String message) {
         open("http://localhost:9999/");
-        $("[data-test-id=name] input.input__control").setValue("Pupkin Vasilii");
-        $("[data-test-id=phone] input.input__control").setValue("+79270000000");
+        $("[data-test-id=name] input.input__control").setValue(name);
+        $("[data-test-id=phone] input.input__control").setValue(phoneNumber);
         $("[data-test-id=agreement]").click();
         $("button").click();
         $(".input_invalid").shouldHave(exactText("Фамилия и имя Имя и Фамилия указаные неверно. Допустимы " +
                                                             "только русские буквы, пробелы и дефисы."));
     }
-    @Test
-    @DisplayName("Поле Телефон заполенно не корректно, ошибка")
-    void shouldTestIrrelevantPhoneNumber() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/IrrelevantPhoneNumber.csv", numLinesToSkip = 1)
+    void shouldTestIrrelevantPhoneNumber(String name, String phoneNumber, String message) {
         open("http://localhost:9999/");
-        $("[data-test-id=name] input.input__control").setValue("Пупкин Василий");
-        $("[data-test-id=phone] input.input__control").setValue("7-927-000-0000");
+        $("[data-test-id=name] input.input__control").setValue(name);
+        $("[data-test-id=phone] input.input__control").setValue(phoneNumber);
         $("[data-test-id=agreement]").click();
         $("button").click();
         $(".input_invalid").shouldHave(exactText("Мобильный телефон Телефон указан неверно. Должно быть 11" +
